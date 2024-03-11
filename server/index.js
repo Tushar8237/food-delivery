823eaa2339bdc1f040ddaf7ceb361bef5a32a9d0
Tushar Suryawanshi <tushar.823737@gmail.com>
2024-03-11 17:04:20 +0530

    add depolymnet script

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ import menuItemRoute from './routes/menuItem.routes.js'
 import addressRoute from './routes/address.routes.js'
 import userRoutes from './routes/user.routes.js'
 import orderRoutes from './routes/order.routes.js'
-
 import cookieParser from 'cookie-parser'
 import fileUpload from 'express-fileupload'
+import path from 'path'
 
 dotenv.config()
 
@@ -18,6 +18,8 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 }).catch((err) => {
     console.log(err)
 })
+
+const __dirname = path.resolve();
 
 const app = express()
 app.use(express.json());
@@ -37,7 +39,11 @@ app.use('/api/restaurant', menuItemRoute)
 app.use('/api', addressRoute)
 app.use('/api', orderRoutes)
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
